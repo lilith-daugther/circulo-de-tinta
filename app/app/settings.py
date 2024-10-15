470bded0f532fa01a10abe6ec9e3559fef80a6ef
Luis Adrian Hernandez <luisac658@outlook.com>
2024-10-15 17:18:39 -0600

    Configuración con las bases de datos en django

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bpx3bx*c$ih8z7mwrr$#=d!k!gns76u%2ly9x=d$7estxzoaz1'
+SECRET_KEY = config('SECRET_KEY')
+
+PORT= config('PORT', default= 8000)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,10 +78,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'db_sql',
+        'PORT': config('POSTGRES_PORT', default= '5432')
     }
 }
+
+MONGO_URL = config('MONGO_URL')
 
 
 # Password validation
